2ed3c1c91b88c9903bc8db80e2a8aaca95cc8de0
K-scorpio <kevin.s.hartley@gmail.com>
2016-08-10 14:06:33 -0600

    possible update to static cells in drawer

--- Local Menu/CuisineViewController.swift
+++ Local Menu/CuisineViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class CuisineViewController: UIViewController {
+class CuisineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
 
-
+    let categories = ["A L L", "M E X I C A N", "I T A L I A N", "C H I N E S E", "B U R G E R S", "J A P E N E S E", "I N D I A N", "C O F F E E", "T H A I", "G R E E K", "S E A F O O D", "O T H E R"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cuisineCell", forIndexPath: indexPath) as! CuisineTableViewCell
+        
+        cell.cuisineLabel.text = categories[indexPath.row]
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
